33c2e062f60d0b97fd042d73ece4d23fdf098650
unknown <jebinrufuz@gmail.com>
2025-02-25 10:42:08 +0530

    Updated a text to test

--- backend/server.js
+++ backend/server.js
@@ -14,5 +14,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is successfully running on http://localhost:${PORT}`);
 });
